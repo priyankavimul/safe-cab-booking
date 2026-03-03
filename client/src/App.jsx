6946ef21408e558fbe207aa4f37d31d1075d5c52
priyankavimul <priyankavimul14@gmail.com>
2026-03-03 15:46:34 +0530

    Added authentication logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Register from './components/Auth/Register';
-import Login from './components/Auth/Login';
-import './App.css';
-
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <nav style={{ padding: "20px", background: "#f0f0f0", marginBottom: "20px" }}>
-          <Link to="/register" style={{ marginRight: "15px" }}>Register</Link>
+      {/* This div class connects to the CSS above */}
+      <div className="app-container"> 
+        <nav>
+          <Link to="/register">Register</Link>
           <Link to="/login">Login</Link>
         </nav>
 
         <Routes>
-          
-        <Route path="/" element={<Register />} /> 
-        <Route path="/register" element={<Register />} />
-
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to="/register" />} />
         </Routes>
       </div>
     </Router>
   );
 }
-
-export default App;
