38dc1574cb97f4491fd2c390c7908ef96195fab6
priyankavimul <priyankavimul14@gmail.com>
2026-03-03 16:26:28 +0530

    Added authentication logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,14 @@
+import React from 'react';
+// This line MUST include 'BrowserRouter as Router' to fix your error
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import './App.css';
+
 function App() {
   return (
     <Router>
-      {/* This div class connects to the CSS above */}
-      <div className="app-container"> 
+      <div className="app-container">
         <nav>
           <Link to="/register">Register</Link>
           <Link to="/login">Login</Link>
@@ -11,9 +17,13 @@ function App() {
         <Routes>
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
+          {/* Redirects to register so you don't see a blank page */}
           <Route path="/" element={<Navigate to="/register" />} />
         </Routes>
       </div>
     </Router>
   );
 }
+
+// This line fixes the 'Does not provide an export named default' error
+export default App;
